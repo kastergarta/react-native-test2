fa2ae67113d8151e001c751e3a2c049688ac8e70
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-06-10 18:36:32 -0400

    custom components - header - start game screen

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const StartGameScreen = props => {
+    return (
+        <View>
+           
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        
+    }
+})
+
+export default StartGameScreen;
