68806e8e9192472c8510633564b01e00e999c5c4
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-06-10 23:49:21 -0400

    card and header components + styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
+
+import Card from '../components/Card'
 
 const StartGameScreen = props => {
     return (
-        <View>
-           
+        <View style={styles.screen}>
+           <Text style={styles.title}>This is it!</Text>
+            <Card style={styles.inputContainer}>              
+               <Text>Select a number</Text>
+               <TextInput />
+               <View style={styles.buttonContainer}>
+                   <Button title='Reset' onPress={() => {}}/>
+                   <Button title='Confirm' onPress={() => {}}/>
+               </View>
+            </Card> 
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     screen: {
-        
+      flex: 1,
+      padding: 10,
+      alignItems: 'center',
+      justifyContent: 'flex-start'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
     }
 })
 
